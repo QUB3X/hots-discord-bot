081cbbbf095b3d862f41bb7207ff0d90852fbda6
Glitch (hots-discord-bot) <none>
2018-05-04 13:15:04 +0000

    ✊💃 Checkpoint ./server.js:433729/496

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ db.serialize(() => {
   }
 })
 
-function addUser () {
+function addUser (discordId, hotslogsId, battleTag) {
   // insert default dreams
   db.serialize(() => {
-    db.run('INSERT INTO Dreams (dream) VALUES ("Find and count some sheep"), ("Climb a really tall mountain"), ("Wash the dishes")');
+    db.run(`INSERT INTO Users (discordId, hotslogsId, battleTag) VALUES (${discordId}, ${hotslogsId}, ${battleTag})`);
   })
 }
 
@@ -28,10 +28,16 @@ bot.on('ready', () => {                                // When the bot is ready
     console.log('Ready!');                             // Log "Ready!"
 });
  
-bot.on('messageCreate', (msg) => {                     // When a message is created
-    if(msg.content.includes('1337')) {                 // If the message content includes "1337"
-        bot.createMessage(msg.channel.id, 'damn it');  // Send a message in the same channel with "damn it"
-    }
+bot.on('messageCreate', (msg) => {
+  // When a message is created
+  if(msg.content.includes('!register')) {
+    msg.createMessage(msg.channel.id, `Ok $(msg.author), what's your BattleTag?`)
+  }
+  if(msg.content.includes('1337')) {
+    // If the message content includes "1337"
+    bot.createMessage(msg.channel.id, 'damn it');
+    // Send a message in the same channel with "damn it"
+  }
 });
  
 bot.connect();                                         // Get the bot to connect to Discord
