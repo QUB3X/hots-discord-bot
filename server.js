bfb6406bd29cb9f29544cee7f90e34415eb213f2
Glitch (hots-discord-bot) <none>
2018-05-06 18:40:14 +0000

    🚝🖖 Checkpoint ./server.js:433729/16

--- server.js
+++ server.js
@@ -94,10 +94,10 @@ bot.on('messageCreate', (msg) => {
       bot.sendChannelTyping(msg.channel.id)
       fetchPlayerData(msg, (player) => {
         bot.createMessage(msg.channel.id, "Hi " + msg.member.username + ", here's your MMR:\n" +
-              "**Team League**\n " + player.teamLeague + "\n" +
-              "**Hero League**\n " + player.heroLeague + "\n" +
-              "**Quick Match**\n " + player.quickMatch + "\n" +
-              "**Unranked Draft**\n " + player.unrankedDraft + "\n")
+              "**Team League**:  " + player.teamLeague + "\n" +
+              "**Hero League**:  " + player.heroLeague + "\n" +
+              "**Quick Match**:  " + player.quickMatch + "\n" +
+              "**Unranked Draft**:  " + player.unrankedDraft + "\n")
       })
     }
   }
